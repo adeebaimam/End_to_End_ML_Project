990204590aa3948a0012316dfc1a339db63a8102
Adeeba Imam <adeebaimam678@gmail.com>
2026-01-23 18:02:40 +0530

    commits

---
+++ src/mlProject/components/data_transformation.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+import sys
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+from src.mlProject.exception import CustomException
+from src.mlProject.logger import logging
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join("artifacts","preprocessor.pkl")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        """This function is responsible for data transformation"""
+        try:
+            numerical_columns = ['writing_score',"reading_score"]
+            categorical_columns = ['gender','race_ethnicity','parental_level_of_education','lunch','test_preparation_course']
+
+            num_pipeline = Pipeline(steps=[
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+            ])
+
+            cat_pipeline = Pipeline(steps=[
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder())
+            ])
+            logging.info("Numerical and Categorical columns:", numerical_columns, categorical_columns)
+
+            preprocessor = ColumnTransformer([
+                ("numerical_pipeline",num_pipeline,numerical_columns),
+                ("categorical")
+            ])
+        except Exception as e:
+            raise CustomException(e, sys)
